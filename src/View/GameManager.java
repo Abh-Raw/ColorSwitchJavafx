f3ea9074d476e04732e80cefe7a79fc308393ccb
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-12-13 22:06:05 +0530

    Added Obstacle 7 -->figure 8

--- src/View/GameManager.java
+++ src/View/GameManager.java
@@ -498,6 +498,60 @@ public class GameManager {
         return obstacles;
     }
 
+    private GameObstacles animateObstacle7(AnchorPane gp, float x, float y) {
+        GameObstacles obstacles = new Obstacle_7();
+
+        Rotate rotation1 = new Rotate();       //1 rotation obj for every component
+        Rotate rotation2 = new Rotate();
+        Rotate rotation3 = new Rotate();
+        Rotate rotation4 = new Rotate();
+        Rotate rotation5 = new Rotate();       //1 rotation obj for every component
+        Rotate rotation6 = new Rotate();
+        Rotate rotation7 = new Rotate();
+        Rotate rotation8 = new Rotate();
+//        Rotate rotation9 = new Rotate();       //1 rotation obj for every component
+//        Rotate rotation10 = new Rotate();
+//        Rotate rotation11 = new Rotate();
+//        Rotate rotation12 = new Rotate();
+
+        obstacles.createObstacle(x, y, start_ball_obj.getStart_ball());  //create obj
+
+        obstacles.getArc_components().get(0).getTransforms().add(rotation1);   //rotation obj added to every component
+        obstacles.getArc_components().get(1).getTransforms().add(rotation2);
+        obstacles.getArc_components().get(2).getTransforms().add(rotation3);
+        obstacles.getArc_components().get(3).getTransforms().add(rotation4);
+        obstacles.getArc_components().get(4).getTransforms().add(rotation5);   //rotation obj added to every component
+        obstacles.getArc_components().get(5).getTransforms().add(rotation6);
+        obstacles.getArc_components().get(6).getTransforms().add(rotation7);
+        obstacles.getArc_components().get(7).getTransforms().add(rotation8);
+//        obstacles.getArc_components().get(8).getTransforms().add(rotation9);
+//        obstacles.getArc_components().get(9).getTransforms().add(rotation10);
+//        obstacles.getArc_components().get(10).getTransforms().add(rotation11);
+//        obstacles.getArc_components().get(11).getTransforms().add(rotation12);
+
+        gp.getChildren().addAll(obstacles.getArc_components());
+
+        start_ball_obj.setBlue_flag(false);     //when we collide with colorswitch, we check next obstacle has which colors and out of those we choose a color for ball
+        start_ball_obj.setRed_flag(false);      //to avoid deadlock
+        start_ball_obj.setGreen_flag(false);
+        start_ball_obj.setYellow_flag(false);
+
+        for(int i = 0; i<obstacles.getArc_components().size(); ++i){  //color components in the next obstacle are set true to avoid deadlock
+            if(obstacles.getArc_components().get(i).getStroke() == Color.BLUE)
+                start_ball_obj.setBlue_flag(true);
+            if(obstacles.getArc_components().get(i).getStroke() == Color.RED)
+                start_ball_obj.setRed_flag(true);
+            if(obstacles.getArc_components().get(i).getStroke() == Color.GREEN)
+                start_ball_obj.setGreen_flag(true);
+            if(obstacles.getArc_components().get(i).getStroke() == Color.YELLOW)
+                start_ball_obj.setYellow_flag(true);
+        }
+
+        obstacles.addAnimation(x, y, gp);
+
+        return obstacles;
+    }
+
 
     private void createSpaceListener(){
         gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -789,7 +843,7 @@ public class GameManager {
     private GameObstacles chooseObstacleRandom(AnchorPane gp, float x, float y){     //creates random obstacles
 
         Random chooseObstacle = new Random();
-        int obstacle_id = chooseObstacle.nextInt(1)+4;
+        int obstacle_id = chooseObstacle.nextInt(1)+7;
         if(obstacle_id==1){
             return animateObstacle1(gp, x, y, null, null);
         }
@@ -812,6 +866,9 @@ public class GameManager {
 
         else if(obstacle_id == 6){
             return animateObstacle6(gp, x, y);
+        }
+        else if(obstacle_id == 7){
+            return animateObstacle7(gp, x, y);
         }
 
          return null;
